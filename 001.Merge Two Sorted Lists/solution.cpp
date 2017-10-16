097c53fdf6806bc0e035c9c85e32ec7c74289482
mytlx <291132618@qq.com>
2017-10-16 23:54:04 +0800

    Update solution.cpp

--- 001.Merge Two Sorted Lists/solution.cpp
+++ 001.Merge Two Sorted Lists/solution.cpp
@@ -1,30 +1,30 @@
-  /**
- 	* Definition for singly-linked list.
- 	* struct ListNode {
- 	*     int val;
- 	*     struct ListNode *next;
- 	* };
-	 */
-	struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) 
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) 
+{
+    struct ListNode head ;
+    struct ListNode *pnew = &head;
+    pnew->next = l1;		    
+    while(l1 != NULL && l2 != NULL)
+    {
+	if(l1->val > l2->val)
 	{
-	    struct ListNode head ;
-	    struct ListNode *pnew = &head;
-	    pnew->next = l1;		    
-	    while(l1 != NULL && l2 != NULL)
-	    {
-	        if(l1->val > l2->val)
-	        {
-	            struct ListNode* temp = NULL;
-	            temp = l2->next;
-	            l2->next = pnew->next;
-	            pnew->next = l2;
-	            l2 = temp;
-	        }
-	        else
-	            l1 = l1->next;
-	        pnew = pnew->next;
-	    }	    
-	    if(l2 != NULL)
-	        pnew->next = l2;	    
-	    return head.next; 
+	    struct ListNode* temp = NULL;
+	    temp = l2->next;
+	    l2->next = pnew->next;
+	    pnew->next = l2;
+	    l2 = temp;
 	}
+	else
+	    l1 = l1->next;
+	pnew = pnew->next;
+    }	    
+    if(l2 != NULL)
+	pnew->next = l2;	    
+    return head.next; 
+}
