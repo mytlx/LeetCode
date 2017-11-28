86e1660d6e3d6524d3704b82a9731bc26304674f
Pure <291132618@qq.com>
2017-11-28 19:44:46 +0800

    Create solution1.h

---
+++ 037.Remove Element/solution1.h
@@ -0,0 +1,23 @@
+#include <vector>
+
+using std::vector; 
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) 
+    {
+        int n = nums.size();
+        int i = 0;
+        while(i < n)
+        {
+            if(nums[i] == val)
+            {
+                nums[i] = nums[n-1];
+                --n;
+            }
+            else
+                ++i;
+        }
+        return n;
+    }
+};
